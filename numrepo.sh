aab73ec44a0642bf7511c7ef8272c62e28ed9170
HeidiJay720 <HeidiJay@yahoo.com>
2020-06-30 14:44:45 -0400

    adding 2

--- numrepo.sh
+++ numrepo.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 # Automation for retrieving a list of my repositories and commitments
 
+USER=HeidiJay; curl "https://api.github.com/HeidiJay/$USER/repos" | grep -o 'git@github.com:HeidiJay/'
