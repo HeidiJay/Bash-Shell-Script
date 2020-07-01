8ea2a75785a4bd6ae40fbeceedf7f467c7d811ce
HeidiJay720 <HeidiJay@yahoo.com>
2020-07-01 09:43:08 -0400

    adding compfile.sh

---
+++ compfile.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# Comparing files for coruption
+# Under construction - Create WordPress site for this - Make sure I understand everything going on here.
+cd public_html/ # Need to create new WordPress with public_html. Old Hosting expired. 
+
+echo "Comparing both htaccess files."
+
+wget http:// # Add the new location here when finished making it.
+# Find out if I need to download software for wget
+if diff .htaccess cleanwphtaccess >/dev/null ; # dev/null is the trash file
+then
+echo "This File Is Safe."
+else
+echo "This File May Be Corrupted!  Renaming It Now!"  && mv .htaccess .htaccess.corrupted  # Look up where .corrupted is at or how I create it.
+fi 
+mv cleanwphtaccess .htaccess # The file is being replaced here
