57070bacd9198208686ae4765a7db498f9802c54
HeidiJay720 <HeidiJay@yahoo.com>
2020-07-01 10:53:58 -0400

    adding compfile2.sh file

---
+++ compfile2.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# Practicing scripts for file comparisons
+# Experimenting different ways to compare files and seeing if scripts work.
+
+cd Heidi-Projects/RPM-Painting
+
+echo "Comparing both RPM-Painting files"
+
+wget git@github.com:HeidiJay/RPM-Painting.git
+
+if diff RPM-Painting cleanfile >/dev/null ; #Experiment 
+then
+echo "This File Is Safe."
+else
+echo "This File May Be Corrupted!"
+fi 
+echo "End!"
