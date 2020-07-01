d2d96c59085c674245c9d192c966e18eda9b2edc
HeidiJay720 <HeidiJay@yahoo.com>
2020-07-01 16:08:15 -0400

    Using comments to break down and understand what is happening

--- hosts.sh
+++ hosts.sh
@@ -3,7 +3,9 @@
 # Copying it and breaking it down to understand what is happing.
 
 echo "CHECKING HOSTS FILE" # message
-if [ -n "$(grep "wordpress.local" /etc/hosts)" ]; # 
+if [ -n "$(grep "wordpress.local" /etc/hosts)" ]; # "$grep" means reach inside and search for patterns in files
+    # "-n" means show matching line number.
+    # reach inside wordpress.local and /ect/hosts and search for patterens and show matching number lines
     then
 		echo "wordpress.local ALREADY EXISTS" # message
 	else
@@ -13,8 +15,10 @@ if [ -n "$(grep "wordpress.local" /etc/hosts)" ]; #
 		# "--" marks the end of options and thus the beginning of arguments.
         # "-c" means read commands from the following string and assign any arguments to the positional parameters
         # "-e" means execute command
-        # 
-        if [ -n "$(grep "wordpress.local" /etc/hosts)" ]; #  
+        # "sudo -- sh -c -e" means As root user, read commands in string and assign any arguments to the parameters and excute those commands 
+        if [ -n "$(grep "wordpress.local" /etc/hosts)" ]; #  # "$grep" means reach inside and search for patterns in files
+         # "-n" means show matching line number.
+         # reach inside wordpress.local and /ect/hosts and search for patterens and show matching number lines
         then
 			echo "wordpress.local SUCCESSFULLY ADDED TO HOSTS FILE"; # message
 		else
