5620f8d44b67f58fae0f494dce165cf69060c503
HeidiJay720 <HeidiJay@yahoo.com>
2020-07-01 08:57:42 -0400

    deleted un-needed file

--- numrepo.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-# Automation for retrieving a list of my repositories and commitments
-
-USER=HeidiJay; curl "https://api.github.com/HeidiJay/$USER/repos" | grep -o 'git@github.com:HeidiJay/'
